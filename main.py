f518bb367e51895052d3bb546faaca934a727f75
ganga <vinodganga178@gmail.com>
2026-05-28 23:33:19 +0530

    fixed backend fully

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from database import SessionLocal, engine
-from models import Task, Base
-
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,35 +11,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Home route
-@app.get("/")
-def home():
-    return {"message": "Backend Running 🚀"}
+tasks = []
 
-# Get all tasks
+# GET all tasks
 @app.get("/tasks")
 def get_tasks():
-    db = SessionLocal()
-
-    tasks = db.query(Task).all()
-
     return tasks
 
-# Create task
+# ADD task
 @app.post("/tasks/{text}")
 def add_task(text: str):
+
     task = {
         "id": len(tasks) + 1,
-        "text": text
+        "text": text,
+        "done": False
     }
 
     tasks.append(task)
 
     return {"message": "Task added"}
+
+# DELETE task
 @app.delete("/tasks/{id}")
 def delete_task(id: int):
+
     global tasks
 
     tasks = [task for task in tasks if task["id"] != id]
 
     return {"message": "Task deleted"}
+
+# TOGGLE DONE
+@app.put("/tasks/{id}")
+def update_task(id: int):
+
+    for task in tasks:
+
+        if task["id"] == id:
+            task["done"] = not task["done"]
+
+    return {"message": "Task updated"}
